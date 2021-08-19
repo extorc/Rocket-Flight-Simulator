3a9e438b6bc5235cec9f49689013a457b14ed665
extorc <mittalshaurya004@gmail.com>
2021-08-19 16:46:40 +0530

    Migration to Surface from Rect

--- rocket.py
+++ rocket.py
@@ -1,11 +1,16 @@
 import pygame as py
 
 class Rocket:
-    def __init__(self,x,y,l,t,screen):
+    def __init__(self,x,y,l,t,screen,color):
         self.x = x
         self.y = y
         self.l = l
         self.t = t
+        self.surface = py.Surface((self.x,self.y))
+        # self.rect = py.Rect(self.l,self.t,self.x,self.y)
         self.screen = screen
+        self.color = color
     def draw(self):
-        py.draw.rect(self.screen,(255,255,255),py.Rect(self.l,self.t,self.x,self.y))
+        self.surface.fill(self.color)
+        self.screen.blit(self.surface,(self.l,self.t))
+        # py.draw.rect(self.screen,self.color,self.rect)
--- sim.py
+++ sim.py
@@ -1,6 +1,6 @@
 import pygame
 from rocket import *
-
+#screen info
 background_colour = (150,150,150)
 (width, height) = (600,600)
 screen = pygame.display.set_mode((width, height))
@@ -8,8 +8,10 @@ pygame.display.set_caption('Simulator')
 screen.fill(background_colour)
 pygame.display.flip()
 running = True
-rocket_dim = 30
-r = Rocket(rocket_dim,rocket_dim,width/2-rocket_dim/2,height/2-rocket_dim/2,screen)
+#rocket info
+rocket_dim_x = 30
+rocket_dim_y = 250
+r = Rocket(rocket_dim_x,rocket_dim_y,width/2-rocket_dim_x/2,height/2-rocket_dim_y/2,screen,(255,255,255))
 
 while running:
   for event in pygame.event.get():
