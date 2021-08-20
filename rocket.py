42633cdfe4ab7ede0cceae63f4e6cd41f52dd1d6
extorc <mittalshaurya004@gmail.com>
2021-08-20 16:43:23 +0530

    Basic Rocket Rotation

--- rocket.py
+++ rocket.py
@@ -6,11 +6,16 @@ class Rocket:
         self.y = y
         self.l = l
         self.t = t
-        self.surface = py.Surface((self.x,self.y))
-        # self.rect = py.Rect(self.l,self.t,self.x,self.y)
+        self.surface = py.Surface((self.x,self.y),py.SRCALPHA)
         self.screen = screen
         self.color = color
-    def draw(self):
         self.surface.fill(self.color)
+        self.rotated_surface = self.surface
+        self.angle = 0
+    def draw(self):
         self.screen.blit(self.surface,(self.l,self.t))
-        # py.draw.rect(self.screen,self.color,self.rect)
+    def rotation(self,angle):
+        self.angle += angle
+        self.rotated_surface = py.transform.rotate(self.surface, self.angle)
+        self.rect = self.rotated_surface.get_rect(center = (self.l + self.x/2,self.t + self.y/2))
+        self.screen.blit(self.rotated_surface, (self.rect.x, self.rect.y))
--- sim.py
+++ sim.py
@@ -1,21 +1,25 @@
 import pygame
 from rocket import *
+
 #screen info
 background_colour = (150,150,150)
 (width, height) = (600,600)
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Simulator')
-screen.fill(background_colour)
 pygame.display.flip()
 running = True
+clock = py.time.Clock()
+
 #rocket info
 rocket_dim_x = 30
 rocket_dim_y = 250
 r = Rocket(rocket_dim_x,rocket_dim_y,width/2-rocket_dim_x/2,height/2-rocket_dim_y/2,screen,(255,255,255))
 
 while running:
+  clock.tick(40)
+  screen.fill(background_colour)
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
-  r.draw()
+  r.rotation()
   py.display.update()
