1a47399257c0491aaacc1e2dfa9dc321fccc041b
extorc <mittalshaurya004@gmail.com>
2021-08-20 18:39:41 +0530

    Simulation Class , Main File

---
+++ main.py
@@ -0,0 +1,4 @@
+from sim import *
+
+sim = Sim((100,100,100),(600,600),"Simulator",30,(255,255,255),180)
+sim.simulate()
--- sim.py
+++ sim.py
@@ -1,24 +1,26 @@
 import pygame
 from rocket import *
 
-#screen info
-background_colour = (150,150,150)
-(width, height) = (600,600)
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Simulator')
-pygame.display.flip()
-running = True
-clock = py.time.Clock()
-fps = 30
-#rocket info
-rocket_dim_x = 30
-rocket_dim_y = 250
-r = Rocket(rocket_dim_x,rocket_dim_y,width/2-rocket_dim_x/2,height/2-rocket_dim_y/2,screen,(255,255,255),fps,90)
-while running:
-  clock.tick(fps)
-  screen.fill(background_colour)
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False
-  r.rotation()
-  py.display.update()
+class Sim:
+    def __init__(self,bgcolor,dim,caption,fps,rocket_color,rotation):
+        self.bgcolor = bgcolor
+        self.screen = pygame.display.set_mode(dim)
+        pygame.display.set_caption(caption)
+        pygame.display.flip()
+        self.running = True
+        self.clock = py.time.Clock()
+        self.fps = fps
+        #rocket info
+        rocket_dim_x = 30
+        rocket_dim_y = 250
+        mid_pos = (dim[0]/2-rocket_dim_x/2,dim[1]/2-rocket_dim_y/2)
+        self.rocket = Rocket(rocket_dim_x,rocket_dim_y,mid_pos[0],mid_pos[1],self.screen,(255,255,255),self.fps,rotation)
+    def simulate(self):
+        while self.running:
+          self.clock.tick(self.fps)
+          self.screen.fill(self.bgcolor)
+          for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+              self.running = False
+          self.rocket.rotation()
+          py.display.update()
